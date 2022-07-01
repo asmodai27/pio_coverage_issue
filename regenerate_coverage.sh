c4274d621bd0cbe113fca49c954b02d9aa109809
asmodai27 <asmodai27@wanadoo.fr>
2022-07-01 23:37:16 +0200

    update regenerate_coverage so the script loops until the bug appears

--- regenerate_coverage.sh
+++ regenerate_coverage.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-rm -rf .pio/build/native
+rm -rf .pio/build/native/*/*/*.gcda
 rm -rf coverage/*
 pio test -e native
+while [[ -s .pio/build/native/test/test_pid_lib/pid_test.gcda && -s .pio/build/native/test/test_units/units_test.gcda ]] ; do
+     rm -rf .pio/build/native/*/*/*.gcda
+     pio test -e native
+done
 lcov --directory .pio/build/native/ --capture --output-file coverage/coverage.info
 lcov ${QUIET} --remove "coverage/coverage.info" \
      --output-file "coverage/coverage_trimmed.info" \
